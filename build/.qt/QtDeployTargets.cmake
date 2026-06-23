aac916c6e715cf46d3108f6fb919d78927d479ec
tianchuangya <2896875067@qq.com>
2026-06-23 17:16:36 +0800

    chore: 添加 .gitignore，移除 build 目录跟踪

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DocMindAI_FILE C:/Users/ASUS/Desktop/DocMind AI/build/DocMindAI.exe)
-set(__QT_DEPLOY_TARGET_DocMindAI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DocMindAI_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
